81ae3cff1fc63bd13e17725f31048fc6f0fd5abd
Himank-P <himankp121@outlook.com>
2025-12-26 21:33:00 +0530

    fixed issue in firebaseadmin.js

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -2,13 +2,8 @@ const admin = require('firebase-admin');
 
 let serviceAccount;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // If we are on Render/Vercel
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  // If we are testing locally
-  serviceAccount = require('./config/serviceAccountKey.json');
-}
+
+ serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
